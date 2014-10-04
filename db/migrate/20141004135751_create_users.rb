69ed476bfe18c2ab1569b4a72fff286e275d21a5
Ian Mason <imasonaz@nau.edu>
2014-10-04 18:11:52 +0000

    Added additional user fields

--- db/migrate/20141004135751_create_users.rb
+++ db/migrate/20141004135751_create_users.rb
@@ -6,7 +6,10 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :oauth_token
       t.datetime :oauth_expires_at
-
+	  t.string :field
+	  t.string :location
+	  t.decimal :rate
+	  
       t.timestamps
     end
   end
